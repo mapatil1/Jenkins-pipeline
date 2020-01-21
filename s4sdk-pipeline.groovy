a9de0030faf02e8d9d2377a0ed72cbdf900b1226
Anil Kumar Yadda <akanilkumar884@gmail.com>
2020-01-21 09:36:57 +0000

    removed backend int test
    Change-Id: Iaff6fcc204163069302064f885e6b5afb16ca633

--- s4sdk-pipeline.groovy
+++ s4sdk-pipeline.groovy
@@ -41,10 +41,10 @@ pipeline {
                     when { expression { commonPipelineEnvironment.configuration.runStage.BACKEND_UNIT_TESTS } }
                     steps { stageUnitTests script: this }
                 }
-                stage("Backend Integration Tests") {
-                    when { expression { commonPipelineEnvironment.configuration.runStage.BACKEND_INTEGRATION_TESTS } }
-                    steps { stageBackendIntegrationTests script: this }
-                }
+                // stage("Backend Integration Tests") {
+                //     when { expression { commonPipelineEnvironment.configuration.runStage.BACKEND_INTEGRATION_TESTS } }
+                //     steps { stageBackendIntegrationTests script: this }
+                // }
                 stage("Frontend Integration Tests") {
                     when { expression { commonPipelineEnvironment.configuration.runStage.FRONTEND_INTEGRATION_TESTS } }
                     steps { stageFrontendIntegrationTests script: this }
