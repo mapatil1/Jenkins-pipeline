d6e7763886419197cbc601fe61d2eeb4988787d0
Anil Kumar Yadda <akanilkumar884@gmail.com>
2020-01-21 11:40:42 +0000

    grovvy file
    Change-Id: I55e05278166a7823e789e083d9fc80f993516d6e

--- s4sdk-pipeline.groovy
+++ s4sdk-pipeline.groovy
@@ -45,14 +45,14 @@ pipeline {
                 //     when { expression { commonPipelineEnvironment.configuration.runStage.BACKEND_INTEGRATION_TESTS } }
                 //     steps { stageBackendIntegrationTests script: this }
                 // }
-                stage("Frontend Integration Tests") {
-                    when { expression { commonPipelineEnvironment.configuration.runStage.FRONTEND_INTEGRATION_TESTS } }
-                    steps { stageFrontendIntegrationTests script: this }
-                }
-                stage("Frontend Unit Tests") {
-                    when { expression { commonPipelineEnvironment.configuration.runStage.FRONTEND_UNIT_TESTS } }
-                    steps { stageFrontendUnitTests script: this }
-                }
+                // stage("Frontend Integration Tests") {
+                //     when { expression { commonPipelineEnvironment.configuration.runStage.FRONTEND_INTEGRATION_TESTS } }
+                //     steps { stageFrontendIntegrationTests script: this }
+                // }
+                // stage("Frontend Unit Tests") {
+                //     when { expression { commonPipelineEnvironment.configuration.runStage.FRONTEND_UNIT_TESTS } }
+                //     steps { stageFrontendUnitTests script: this }
+                // }
                 // stage("NPM Dependency Audit") {
                 //     when { expression { commonPipelineEnvironment.configuration.runStage.NPM_AUDIT } }
                 //     steps { stageNpmAudit script: this }
@@ -60,19 +60,19 @@ pipeline {
             }
         }
 
-        stage('Remote Tests') {
-            when { expression { commonPipelineEnvironment.configuration.runStage.REMOTE_TESTS } }
-            parallel {
-                stage("End to End Tests") {
-                    when { expression { commonPipelineEnvironment.configuration.runStage.E2E_TESTS } }
-                    steps { stageEndToEndTests script: this }
-                }
-                stage("Performance Tests") {
-                    when { expression { commonPipelineEnvironment.configuration.runStage.PERFORMANCE_TESTS } }
-                    steps { stagePerformanceTests script: this }
-                }
-            }
-        }
+        // stage('Remote Tests') {
+        //     when { expression { commonPipelineEnvironment.configuration.runStage.REMOTE_TESTS } }
+        //     parallel {
+        //         stage("End to End Tests") {
+        //             when { expression { commonPipelineEnvironment.configuration.runStage.E2E_TESTS } }
+        //             steps { stageEndToEndTests script: this }
+        //         }
+        //         stage("Performance Tests") {
+        //             when { expression { commonPipelineEnvironment.configuration.runStage.PERFORMANCE_TESTS } }
+        //             steps { stagePerformanceTests script: this }
+        //         }
+        //     }
+        // }
 
         // stage('Quality Checks') {
         //     when { expression { commonPipelineEnvironment.configuration.runStage.QUALITY_CHECKS } }
@@ -82,43 +82,43 @@ pipeline {
         //     }
         // }
 
-        stage('Third-party Checks') {
-            when { expression { commonPipelineEnvironment.configuration.runStage.THIRD_PARTY_CHECKS } }
-            parallel {
-                stage("Checkmarx Scan") {
-                    when { expression { commonPipelineEnvironment.configuration.runStage.CHECKMARX_SCAN } }
-                    steps { stageCheckmarxScan script: this }
-                }
-                stage("WhiteSource Scan") {
-                    when { expression { commonPipelineEnvironment.configuration.runStage.WHITESOURCE_SCAN } }
-                    steps { stageWhitesourceScan script: this }
-                }
-                stage("SourceClear Scan") {
-                    when { expression { commonPipelineEnvironment.configuration.runStage.SOURCE_CLEAR_SCAN } }
-                    steps { stageSourceClearScan script: this }
-                }
-                stage("Fortify Scan") {
-                    when { expression { commonPipelineEnvironment.configuration.runStage.FORTIFY_SCAN } }
-                    steps { stageFortifyScan script: this }
-                }
-                stage("Additional Tools") {
-                    when { expression { commonPipelineEnvironment.configuration.runStage.ADDITIONAL_TOOLS } }
-                    steps { stageAdditionalTools script: this }
-                }
-                stage('SonarQube Scan'){
-                    when { expression { commonPipelineEnvironment.configuration.runStage.SONARQUBE_SCAN } }
-                    steps { stageSonarQubeScan script: this }
-                }
-            }
-        }
+        // stage('Third-party Checks') {
+        //     when { expression { commonPipelineEnvironment.configuration.runStage.THIRD_PARTY_CHECKS } }
+        //     parallel {
+        //         stage("Checkmarx Scan") {
+        //             when { expression { commonPipelineEnvironment.configuration.runStage.CHECKMARX_SCAN } }
+        //             steps { stageCheckmarxScan script: this }
+        //         }
+        //         stage("WhiteSource Scan") {
+        //             when { expression { commonPipelineEnvironment.configuration.runStage.WHITESOURCE_SCAN } }
+        //             steps { stageWhitesourceScan script: this }
+        //         }
+        //         stage("SourceClear Scan") {
+        //             when { expression { commonPipelineEnvironment.configuration.runStage.SOURCE_CLEAR_SCAN } }
+        //             steps { stageSourceClearScan script: this }
+        //         }
+        //         stage("Fortify Scan") {
+        //             when { expression { commonPipelineEnvironment.configuration.runStage.FORTIFY_SCAN } }
+        //             steps { stageFortifyScan script: this }
+        //         }
+        //         stage("Additional Tools") {
+        //             when { expression { commonPipelineEnvironment.configuration.runStage.ADDITIONAL_TOOLS } }
+        //             steps { stageAdditionalTools script: this }
+        //         }
+        //         stage('SonarQube Scan'){
+        //             when { expression { commonPipelineEnvironment.configuration.runStage.SONARQUBE_SCAN } }
+        //             steps { stageSonarQubeScan script: this }
+        //         }
+        //     }
+        // }
 
-        stage('Artifact Deployment') {
-            when { expression { commonPipelineEnvironment.configuration.runStage.ARTIFACT_DEPLOYMENT } }
-            steps {
-                milestone 70
-                stageArtifactDeployment script: this
-            }
-        }
+        // stage('Artifact Deployment') {
+        //     when { expression { commonPipelineEnvironment.configuration.runStage.ARTIFACT_DEPLOYMENT } }
+        //     steps {
+        //         milestone 70
+        //         stageArtifactDeployment script: this
+        //     }
+        // }
 
         stage('Production Deployment') {
             when { expression { commonPipelineEnvironment.configuration.runStage.PRODUCTION_DEPLOYMENT } }
